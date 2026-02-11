6b961e1ab77025adb806c586693e7817ffa7ef54
Atur Aritonang <mangatur.aritonang@gmail.com>
2026-02-11 07:45:46 +0000

    Add filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 
 # st.title("🎈 My new app")
@@ -7,10 +8,24 @@ import streamlit as st
 
 import pandas as pd
 
+st.title('Survey')
+
 st.write("""
 # My first app
 Hello *world!*
 """)
+folder_path = 'data' # Relative path to your folder
+file_name = 'SurveyDummy.csv'
+full_path = os.path.join(folder_path, file_name)
 
-df = pd.read_csv("./data/SurveyDummy.csv")
-st.line_chart(df)
+df = pd.read_csv(full_path, header=0)
+serviceLineOptions = df['Service Line'].unique().tolist()
+serviceLine = st.sidebar.selectbox('Service line', options=serviceLineOptions, key = 'serviceLine')
+
+bandOptions = df['Band'].unique().tolist()
+band = st.sidebar.selectbox('Band', options=bandOptions, key = 'band')
+
+genderOptions = df['Gender'].unique().tolist()
+gender = st.sidebar.selectbox('Gender', options=genderOptions, key = 'gender')
+
+st.line_chart(df, x='Service Line', y='Answer1')
