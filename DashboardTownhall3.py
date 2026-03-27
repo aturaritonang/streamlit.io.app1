c592b0da682d54fa27c638420cfac19639df57d7
Atur Aritonang <mangatur.aritonang@gmail.com>
2026-03-28 00:09:02 +0700

    Quiz option sudah bisa masuk

--- DashboardTownhall3.py
+++ DashboardTownhall3.py
@@ -4,6 +4,19 @@ import altair as alt
 import gspread
 from google.oauth2.service_account import Credentials
 
+# st.markdown("""
+#     <style>
+#     .stDataFrame td {
+#         white-space: normal !important;
+#         word-wrap: break-word !important;
+#     }
+#     .stDataFrame th {
+#         white-space: normal !important;
+#         word-wrap: break-word !important;
+#     }
+#     </style>
+# """, unsafe_allow_html=True)
+
 st.title("Survey Dashboard")
 
 # =====================
@@ -21,8 +34,10 @@ creds = Credentials.from_service_account_info(
 
 client = gspread.authorize(creds)
 
-sheet = client.open("Result Townhall").sheet1
+dfQuiz = pd.DataFrame(client.open("Master Quiz").worksheet("Trial-Id").get_all_records())
 
+
+sheet = client.open("Result Townhall").sheet1
 data = sheet.get_all_records()
 df = pd.DataFrame(data)
 
@@ -38,6 +53,8 @@ answer_cols = [
     "2 Needs Improvement",
     "1 Does Not Meet Expectations"
 ]
+
+answer_cols_alpabet = ["A", "B", "C", "D", "E"]
 
 # Convert TRUE/FALSE → 1/0
 df[answer_cols] = df[answer_cols].replace({
@@ -56,6 +73,8 @@ for quiz in quiz_list:
     st.text(f"Quiz: {quiz}")
     
     quiz_df = df[df["Quiz"] == quiz]
+
+    master_quiz_df = dfQuiz[dfQuiz["Quiz"] == quiz]
 
     totals = quiz_df[answer_cols].sum().reset_index()
     totals.columns = ["Level", "Answer"]
@@ -78,7 +97,19 @@ for quiz in quiz_list:
     # =====================
     # TABLE (Level vs Answer)
     # =====================
-    st.dataframe(totals, use_container_width=True, hide_index=True)
+    newTotals = totals
+    # newTotals["Level"] = master_quiz_df[newTotals["Level:N"]]
+    # st.text(newTotals["Level"].iloc[0])
+    # newTotals["Level"] = master_quiz_df[newTotals["Level"].iloc[0]]
+    # st.dataframe(master_quiz_df, use_container_width=True, hide_index=True)
+    # st.dataframe(master_quiz_df["5 Role Model"], use_container_width=True, hide_index=True)
+
+    newTotals["Level"] = newTotals["Level"].apply(
+        lambda L: L + ":\n" + master_quiz_df.iloc[0][L] if L in master_quiz_df.columns else ""
+    )
+    
+    # st.dataframe(newTotals, use_container_width=False, hide_index=True)
+    st.table(newTotals.reset_index(drop=True))
 
     st.markdown("---")
 
